ac12a12f6c71101e08d5bda3cc0c6f98ce6f32ca
hooke <cookmycode@gmail.com>
2024-12-06 21:47:28 +0800

    ...

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'green',
+  webDir: 'dist'
+};
+
+export default config;
